5bd4b04ee00d41ce8790afb4dececb7237490033
sjlvanq <sjlvanq@gmail.com>
2025-12-26 22:42:59 -0300

    refactor: uncomment and enable security logic

--- src/main/java/com/churncheck/api/infra/security/SecurityFilter.java
+++ src/main/java/com/churncheck/api/infra/security/SecurityFilter.java
@@ -1,52 +1,52 @@
-// package com.churncheck.api.infra.security;
+ package com.churncheck.api.infra.security;
 
-// import java.io.IOException;
+ import java.io.IOException;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import org.springframework.security.core.context.SecurityContextHolder;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Component;
-// import org.springframework.web.filter.OncePerRequestFilter;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+ import org.springframework.security.core.context.SecurityContextHolder;
+ import org.springframework.security.core.userdetails.UsernameNotFoundException;
+ import org.springframework.stereotype.Component;
+ import org.springframework.web.filter.OncePerRequestFilter;
 
-// import jakarta.servlet.FilterChain;
-// import jakarta.servlet.ServletException;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import com.churncheck.api.domain.user.UserRepository;
+ import jakarta.servlet.FilterChain;
+ import jakarta.servlet.ServletException;
+ import jakarta.servlet.http.HttpServletRequest;
+ import jakarta.servlet.http.HttpServletResponse;
+ import com.churncheck.api.domain.user.UserRepository;
 
-// @Component
-// public class SecurityFilter extends OncePerRequestFilter {
+ @Component
+ public class SecurityFilter extends OncePerRequestFilter {
 
-//     @Autowired
-//     private TokenService tokenService;
+     @Autowired
+     private TokenService tokenService;
 
-//     @Autowired
-//     private UserRepository userRepository;
+     @Autowired
+     private UserRepository userRepository;
 
-//     @Override
-//     protected void doFilterInternal(
-//     		HttpServletRequest request,
-//     		HttpServletResponse response,
-//     		FilterChain filterChain) throws ServletException, IOException {
+     @Override
+     protected void doFilterInternal(
+     		HttpServletRequest request,
+     		HttpServletResponse response,
+     		FilterChain filterChain) throws ServletException, IOException {
 
-//         String authHeader = request.getHeader("Authorization");
-//         if (authHeader != null && authHeader.length() > "BEARER ".length() && 
-//         		authHeader.substring(0, "BEARER ".length()).equalsIgnoreCase("BEARER ")) {
-//             String token = authHeader.split(" ")[1].trim();
-//             String userEmail = tokenService.getSubject(token);
+         String authHeader = request.getHeader("Authorization");
+         if (authHeader != null && authHeader.length() > "BEARER ".length() && 
+         		authHeader.substring(0, "BEARER ".length()).equalsIgnoreCase("BEARER ")) {
+             String token = authHeader.split(" ")[1].trim();
+             String userEmail = tokenService.getSubject(token);
 
-//             if (userEmail != null) {
-//                 // Token is valid
-//                 // TODO: Decidir personalización de UsernameNotFoundException
-//             	AuthUser authUser = new AuthUser(userRepository.findByEmailWithRoles(userEmail)
-//             			.orElseThrow(()->new UsernameNotFoundException("User not found")));
-//                 UsernamePasswordAuthenticationToken authentication =
-//                 		new UsernamePasswordAuthenticationToken(authUser, null, authUser.getAuthorities());
-//                 SecurityContextHolder.getContext().setAuthentication(authentication);
-//             }
-//         }
+             if (userEmail != null) {
+                 // Token is valid
+                 // TODO: Decidir personalización de UsernameNotFoundException
+             	AuthUser authUser = new AuthUser(userRepository.findByEmailWithRoles(userEmail)
+             			.orElseThrow(()->new UsernameNotFoundException("User not found")));
+                 UsernamePasswordAuthenticationToken authentication =
+                 		new UsernamePasswordAuthenticationToken(authUser, null, authUser.getAuthorities());
+                 SecurityContextHolder.getContext().setAuthentication(authentication);
+             }
+         }
 
-//         filterChain.doFilter(request, response);
-//     }
-// }
+         filterChain.doFilter(request, response);
+     }
+ }
