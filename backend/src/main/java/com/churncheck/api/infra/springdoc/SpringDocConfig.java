a61f1c7d42647b50a76acc7124b882ee0339f6d7
sjlvanq <sjlvanq@gmail.com>
2025-12-28 21:25:22 +0000

    fix: enable bearer authorization header in swagger ui

--- backend/src/main/java/com/churncheck/api/infra/springdoc/SpringDocConfig.java
+++ backend/src/main/java/com/churncheck/api/infra/springdoc/SpringDocConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
@@ -15,6 +16,10 @@ public class SpringDocConfig {
         return new OpenAPI()
             .components(new Components()
             .addSecuritySchemes("bearer-key",
-            	new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")))
+        .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
     }
 }
