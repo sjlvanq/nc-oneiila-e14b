d63f2cf947cb09775b2f1f50aa26ec63f800c843
sjlvanq <sjlvanq@gmail.com>
2025-12-28 21:27:54 +0000

    feat: add test controller

---
+++ backend/src/main/java/com/churncheck/api/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.churncheck.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<String> getTest(){
+    	return ResponseEntity.ok("Hola mundo");
+    }
+}
