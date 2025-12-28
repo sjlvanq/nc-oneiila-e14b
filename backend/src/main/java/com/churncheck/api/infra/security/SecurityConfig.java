e005902b2cf2654b1acdc7dee18050f952cc2cb7
sjlvanq <sjlvanq@gmail.com>
2025-12-28 19:44:14 +0000

    refactor: update security configuration for H2 console and public endpoints

--- backend/src/main/java/com/churncheck/api/infra/security/SecurityConfig.java
+++ backend/src/main/java/com/churncheck/api/infra/security/SecurityConfig.java
@@ -1,19 +1,21 @@
  package com.churncheck.api.infra.security;
 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
- import org.springframework.http.HttpMethod;
- import org.springframework.security.authentication.AuthenticationManager;
- import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
- import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
- import org.springframework.security.config.annotation.web.builders.HttpSecurity;
- import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
- import org.springframework.security.config.http.SessionCreationPolicy;
- import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
- import org.springframework.security.crypto.password.PasswordEncoder;
- import org.springframework.security.web.SecurityFilterChain;
- import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
  @Configuration
  @EnableWebSecurity
@@ -28,13 +30,15 @@
  		return http
  				.csrf(c->c.disable())
  				//.formLogin(form -> form.disable())
+ 				.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // //TODO Quitar en producción - Frames de UI H2
  				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
  				.authorizeHttpRequests((request) ->
  					request
+ 						.requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/h2-console/**")).permitAll() //TODO: Quitar en producción
  						.requestMatchers(HttpMethod.POST, "/login").permitAll()
  						.requestMatchers("/clients").permitAll()
- 						//.requestMatchers("/swagger/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+ 						.requestMatchers("/swagger/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs", "/v3/api-docs/**").permitAll()
  						.anyRequest().authenticated()
  				)
 
