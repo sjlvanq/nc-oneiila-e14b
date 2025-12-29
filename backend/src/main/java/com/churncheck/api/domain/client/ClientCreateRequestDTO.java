07d0051179181e70f027e3e932e9d78cbace8b57
criseida0310 <89651243+criseida0310@users.noreply.github.com>
2025-12-29 13:32:34 -0600

    feat: align ClientCreateRequestDTO with gym churn schema

--- backend/src/main/java/com/churncheck/api/domain/client/ClientCreateRequestDTO.java
+++ backend/src/main/java/com/churncheck/api/domain/client/ClientCreateRequestDTO.java
@@ -1,28 +1,115 @@
 package com.churncheck.api.domain.client;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
-public record ClientCreateRequestDTO(
-    @NotBlank
-    String clientName,
+public class ClientCreateRequestDTO {
 
-    @Email
-    @NotBlank
-    String clientEmail,
-    
-    @NotNull
-    Gender gender,
+    private String clientName;
+    private String gender;
+    private Integer nearLocation;
+    private Integer partnerEmployee;
+    private Integer promoFriends;
+    private String clientPhone;
+    private Integer age;
+    private Integer contractPeriod;
+    private Integer monthToEndContract;
+    private Integer lifetimeMonths;
+    private BigDecimal avgClassFrequencyTotal;
+    private BigDecimal avgClassFrequencyCurrentMonth;
 
-    @NotBlank
-    String clientPhone,
+    public String getClientName() {
+        return clientName;
+    }
 
-    Integer nearLocation,
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
 
-    @NotNull
-    Integer age
+    public String getGender() {
+        return gender;
+    }
 
-) {
-    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getNearLocation() {
+        return nearLocation;
+    }
+
+    public void setNearLocation(Integer nearLocation) {
+        this.nearLocation = nearLocation;
+    }
+
+    public Integer getPartnerEmployee() {
+        return partnerEmployee;
+    }
+
+    public void setPartnerEmployee(Integer partnerEmployee) {
+        this.partnerEmployee = partnerEmployee;
+    }
+
+    public Integer getPromoFriends() {
+        return promoFriends;
+    }
+
+    public void setPromoFriends(Integer promoFriends) {
+        this.promoFriends = promoFriends;
+    }
+
+    public String getClientPhone() {
+        return clientPhone;
+    }
+
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getContractPeriod() {
+        return contractPeriod;
+    }
+
+    public void setContractPeriod(Integer contractPeriod) {
+        this.contractPeriod = contractPeriod;
+    }
+
+    public Integer getMonthToEndContract() {
+        return monthToEndContract;
+    }
+
+    public void setMonthToEndContract(Integer monthToEndContract) {
+        this.monthToEndContract = monthToEndContract;
+    }
+
+    public Integer getLifetimeMonths() {
+        return lifetimeMonths;
+    }
+
+    public void setLifetimeMonths(Integer lifetimeMonths) {
+        this.lifetimeMonths = lifetimeMonths;
+    }
+
+    public BigDecimal getAvgClassFrequencyTotal() {
+        return avgClassFrequencyTotal;
+    }
+
+    public void setAvgClassFrequencyTotal(BigDecimal avgClassFrequencyTotal) {
+        this.avgClassFrequencyTotal = avgClassFrequencyTotal;
+    }
+
+    public BigDecimal getAvgClassFrequencyCurrentMonth() {
+        return avgClassFrequencyCurrentMonth;
+    }
+
+    public void setAvgClassFrequencyCurrentMonth(BigDecimal avgClassFrequencyCurrentMonth) {
+        this.avgClassFrequencyCurrentMonth = avgClassFrequencyCurrentMonth;
+    }
 }
