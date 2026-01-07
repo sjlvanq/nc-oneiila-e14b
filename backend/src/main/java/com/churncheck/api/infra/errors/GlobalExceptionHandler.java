f8ffd78b664e5424de261211d249e710abc8a745
sjlvanq <sjlvanq@gmail.com>
2026-01-07 05:26:42 +0000

    feat: handle login authentication errors in GlobalExceptionHandler

--- backend/src/main/java/com/churncheck/api/infra/errors/GlobalExceptionHandler.java
+++ backend/src/main/java/com/churncheck/api/infra/errors/GlobalExceptionHandler.java
@@ -1,8 +1,20 @@
 package com.churncheck.api.infra.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.churncheck.api.infra.errors.dto.ErrorStatusResponseDTO;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorStatusResponseDTO> handleAccessDenied(UsernameNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+                new ErrorStatusResponseDTO(
+                        ErrorStatusResponseCodes.UNAUTHORIZED_401, 
+                        "Email o contraseña incorrecto"));
+    }
 }
