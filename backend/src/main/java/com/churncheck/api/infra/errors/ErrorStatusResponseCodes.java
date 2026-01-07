1161d29b13e58ce366276ea6dbb0e94f9b0e0e09
sjlvanq <sjlvanq@gmail.com>
2026-01-07 05:21:08 +0000

    refactor: add fields and getters to ErrorStatusResponseCodes enum

--- backend/src/main/java/com/churncheck/api/infra/errors/ErrorStatusResponseCodes.java
+++ backend/src/main/java/com/churncheck/api/infra/errors/ErrorStatusResponseCodes.java
@@ -1,13 +1,29 @@
 package com.churncheck.api.infra.errors;
 
 public enum ErrorStatusResponseCodes {
-	BAD_CREDENTIALS_401,
-	BAD_PATHVARIABLE_400,
-	BAD_REQUEST_400,
-	CONFLICT_409,
-	FORBIDDEN_403,
-	LOCKED_423, 
-	MALFORMED_400,
-	NOT_FOUND_404, 
-	UNAUTHORIZED_401
+	BAD_CREDENTIALS_401("Bad Credentials", 401),
+	BAD_PATHVARIABLE_400("Bad Path Variable", 400),
+	BAD_REQUEST_400("Bad Request", 400),
+	CONFLICT_409("Conflict", 409),
+	FORBIDDEN_403("Forbidden", 403),
+	LOCKED_423("Locked", 423),
+	MALFORMED_400("Malformed", 400),
+	NOT_FOUND_404("Not Found", 404),
+	UNAUTHORIZED_401("Unauthorized", 401);
+
+	private final String desc;
+	private final Integer status;
+
+	ErrorStatusResponseCodes(String desc, int status) {
+        this.desc = desc;
+        this.status = status;
+    }
+
+	public String getDesc() {
+        return desc;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
 }
