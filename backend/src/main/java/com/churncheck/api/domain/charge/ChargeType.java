d3882fb161b8510c72132f994d34c7f3fa62aec3
sjlvanq <sjlvanq@gmail.com>
2026-01-08 06:17:49 +0000

    fix: change charge type column name to description

--- backend/src/main/java/com/churncheck/api/domain/charge/ChargeType.java
+++ backend/src/main/java/com/churncheck/api/domain/charge/ChargeType.java
@@ -14,6 +14,6 @@ public class ChargeType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "charge_type_name")
-    private String name;
+    @Column(name = "description")
+    private String description;
 }
