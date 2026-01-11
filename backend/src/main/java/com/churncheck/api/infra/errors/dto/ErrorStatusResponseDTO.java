c5dd2a2f7a791df08426efb2397d68dfe74effa6
sjlvanq <sjlvanq@gmail.com>
2026-01-11 16:47:49 +0000

    refactor: update error status response dto and add validation logic

--- backend/src/main/java/com/churncheck/api/infra/errors/dto/ErrorStatusResponseDTO.java
+++ backend/src/main/java/com/churncheck/api/infra/errors/dto/ErrorStatusResponseDTO.java
@@ -2,23 +2,30 @@ package com.churncheck.api.infra.errors.dto;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
 
 import com.churncheck.api.infra.errors.ErrorStatusResponseCodes;
 
 public record ErrorStatusResponseDTO(
 	Instant timestamp,
 	Integer status,
-	ErrorStatusResponseCodes error,
+	String desc,
 	String message,
-	List<ErrorStatusResponseFieldDTO> fields
-) {
+	List<ErrorStatusResponseFieldDTO> fields) {
+    
+    public ErrorStatusResponseDTO {
+        timestamp = Instant.now();
+        Objects.requireNonNull(status, "Código de estado del error no puede ser null");
+        Objects.requireNonNull(desc, "Descripción del código de estado del error no puede ser null");
+        fields = (fields == null) ? List.of() : List.copyOf(fields);
+    }
+    
 	public ErrorStatusResponseDTO(ErrorStatusResponseCodes errorStatusResponseCode, String message){
-		this(
-			Instant.now(), 
-			errorStatusResponseCode.getStatus(), 
-			errorStatusResponseCode,
-			message, 
-			List.of()
-		);
+		this(null, errorStatusResponseCode.getStatus(), errorStatusResponseCode.getDesc(), message, null);
 	}
+
+    public ErrorStatusResponseDTO(ErrorStatusResponseCodes errorStatusResponseCode, String message, List<ErrorStatusResponseFieldDTO> fields){
+        this(null, errorStatusResponseCode.getStatus(), errorStatusResponseCode.getDesc(), message, fields);
+    }
+    
 }
