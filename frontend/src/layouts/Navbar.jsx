9cad8afa0357ee53cce04791630cdf4c6b1ac8f2
sjlvanq <sjlvanq@gmail.com>
2026-01-17 18:44:57 +0000

    feat: implement authentication system and protected routes

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <nav>
-            <Link to="/">Home</Link> | <Link to="/perfil">Mi Perfil</Link>
+            <Link to="/">Home</Link> | <Link to="/login">Login</Link> | <Link to="/dashboard">Dashboard</Link>
         </nav>
     );
 }
---
+++ frontend/src/pages/PageDashboard.jsx
@@ -0,0 +1,7 @@
+export default function PageDashboard(){
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
