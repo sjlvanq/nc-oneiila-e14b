8f978576dbedc13f91da1e01fd4bd7b23eabecbb
luis-cm-linux <115508520+luiscm17@users.noreply.github.com>
2026-01-17 02:21:44 +0000

    feat: add getters and setters to Partner entity

--- backend/src/main/java/com/churncheck/api/domain/partner/Partner.java
+++ backend/src/main/java/com/churncheck/api/domain/partner/Partner.java
@@ -13,4 +13,16 @@ public class Partner {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
 }
