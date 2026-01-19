de95d61b028a675be0b58acf8658767f24bc7ebe
luis-cm-b460 < 115508520+LuisCM17@users.noreply.github.com>
2026-01-19 16:05:16 -0400

    feat: update api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setupAuthInterceptor } from '@/interceptors/auth.interceptor';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080', // Base URL
+    baseURL: 'http://172.171.120.208:8080', // Base URL
 });
 
 setupAuthInterceptor(api);
