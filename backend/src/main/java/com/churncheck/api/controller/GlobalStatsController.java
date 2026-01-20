de107f09dbec303aa42aab3b200d555b863ae9c8
luis-cm-linux <115508520+luiscm17@users.noreply.github.com>
2026-01-20 19:56:04 -0400

    fix: add missing CrossOrigin import in GlobalStatsController

--- backend/src/main/java/com/churncheck/api/controller/GlobalStatsController.java
+++ backend/src/main/java/com/churncheck/api/controller/GlobalStatsController.java
@@ -1,6 +1,7 @@
 package com.churncheck.api.controller;
 
 import com.churncheck.api.service.GlobalStatsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
