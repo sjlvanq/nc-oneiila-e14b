cc41df6b2393b3786b006999845b3e3b1b59bef2
sjlvanq <sjlvanq@gmail.com>
2026-01-20 02:12:26 -0300

    feat: unprotect dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,16 +26,11 @@ const router = createBrowserRouter([
         path: "login",
         element: <PageLogin />,
       },
-      // Protected Routes
       {
-        element: <ProtectedRoute />,
-        children: [
-          {
-            path: "dashboard",
-            element: <PageDashboard />,
-          },
-        ],
+        path: "dashboard",
+        element: <PageDashboard />,
       },
+      // Protected Routes
       {
         path: "login", // Login is public
         element: <PageLogin />,
