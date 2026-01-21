844002d122f6a828d046a9dc24c3c586f62b6bf8
luis-cm-linux <115508520+luiscm17@users.noreply.github.com>
2026-01-21 00:32:58 -0400

    feat: implement dynamic titles in all pages

--- frontend/src/pages/PageLogin.jsx
+++ frontend/src/pages/PageLogin.jsx
@@ -1,8 +1,10 @@
 import Login from '@/components/Login'
 import styles from '@/styles/pages/PageLogin.module.css';
 import logo from '@/assets/img/logo-churncheck.png'
+import { useDocumentTitle } from '@/hooks/useDocumentTitle';
 
 export default function PageLogin() {
+    useDocumentTitle('Iniciar Sesión');
     return (
         <div className={styles.loginWrapper}>
             <div className={styles.loginWrapper}>
