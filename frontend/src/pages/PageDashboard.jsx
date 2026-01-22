9d7a7a192107adfed7e0037102802c24d15d958a
MiguelFigueroa02 <deusmus2@gmail.com>
2026-01-22 00:37:09 -0600

    feat: Addition of AttendanceChart.jsx  for dashboards of clients

--- frontend/src/pages/PageDashboard.jsx
+++ frontend/src/pages/PageDashboard.jsx
@@ -1,7 +1,10 @@
 import DniSearch from '@/components/DniSearch';
 import GlobalStats from '../components/GlobalStats';
+import AttendanceChart from "../components/AttendanceChart.jsx";
+import { useState } from 'react';
 
 export default function PageDashboard() {
+    const [selectedClient, setSelectedClient] = useState(null);
     return (
         <div className="dashboard-page">
             <section className="hero" style={{ height: 'auto', paddingTop: '100px', paddingLeft: ' 80px' }}>
@@ -14,12 +17,21 @@ export default function PageDashboard() {
             <GlobalStats />
 
             <div className="container">
-                <DniSearch />
+                <DniSearch onClientFound={setSelectedClient} />
 
                 {/* Las tarjetas de KPI se agregarán aquí en el futuro */}
                 <div style={{ marginTop: '40px', color: '#6b7280', textAlign: 'center' }}>
                     <p>ChurnCheck</p>
                 </div>
+            </div>
+            <div>
+                {selectedClient && (
+                    <>
+                    <h2 style={{alignContent:'center', textAlign:'center', fontSize: '50px'}}>Dashboard</h2>
+                    <AttendanceChart clientId={selectedClient.id} />
+                    </>
+                    
+                )}
             </div>
         </div>
     )
